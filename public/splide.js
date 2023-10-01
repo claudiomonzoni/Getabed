f1d3eb5750afaa1cde841107cbd22af4d4078e71
claudiomonzoni <claudiomonzoni@hotmail.com>
2023-10-01 10:13:22 -0600

    seccion de dominios

--- public/splide.js
+++ public/splide.js
@@ -6,25 +6,27 @@ export function slideHome() {
   document.addEventListener("DOMContentLoaded", function () {
     var splide = new Splide(".splide", {
       type: "loop",
+      speed: 1000,
+      autoplay: true,
+      interval: 6000,
+      pagination: false,
+      wheel: true,
       perPage: "2",
       perMove: 1,
       gap: "1rem",
-      padding: { left: '4rem', right: '0rem' },
-        breakpoints: {
-          640: {
-            perPage: 1,
-            padding: { left: '1rem', right: '4rem' },
-          },
+      padding: { left: "4rem", right: "0rem" },
+      breakpoints: {
+        640: {
+          perPage: 1,
+          padding: { left: "1rem", right: "4rem" },
         },
-        classes: {
-          arrows: 'splide__arrows your-class-arrows',
-          arrow : 'splide__arrow your-class-arrow',
-          prev  : 'splide__arrow--prev btn-sinicono',
-          next  : 'splide__arrow--next btn-sinicono',
-        },
-    
-      
-     
+      },
+      classes: {
+        arrows: "splide__arrows your-class-arrows",
+        arrow: "splide__arrow your-class-arrow",
+        prev: "splide__arrow--prev btn-sinicono",
+        next: "splide__arrow--next btn-sinicono",
+      },
     });
     splide.mount();
   });
